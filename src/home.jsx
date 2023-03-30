abbd07003067d0ff8eccf19638d771e819af5c6a
Keeley Gardner <116581328+keeley1@users.noreply.github.com>
2023-03-30 18:08:33 +0100

    Adding content to home page

--- src/home.jsx
+++ src/home.jsx
@@ -1,11 +1,28 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
         <>
         <div className="home-title-container">
             <h1 className="home-title">Welcome to Travelpedia</h1>
-            <p>homepage</p>
+        </div>
+        <div className="content-container">
+            <div className="home-content-block-1">
+                <img width="400px" src="https://b2003130.smushcdn.com/2003130/wp-content/uploads/2018/06/Caribbean-hammock-beach-1024x683.jpg?lossy=1&strip=1&webp=1"></img>
+                <div className="home-text-1">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                    <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+                </div>
+            </div>
+            <div className="home-content-block-2">
+                <h1>How can we help?</h1>
+                <div className="home-buttons">
+                    <Link to="/travel-recs" className="link-button"><p className="home-button-1">Not sure where to travel to? Take our travel quiz to receive a personalised location recommendation!</p></Link>
+                    <Link to="/locations" className="link-button"><p className="home-button-1">Want to know more before you go? Take a look at our most popular destinations and learn more about them!</p></Link>
+                </div>
+            </div>
         </div>
         </>
     )
--- src/locations.jsx
+++ src/locations.jsx
@@ -21,9 +21,44 @@ const Locations = () => {
       title: "Australia",
       image: "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iZDshdZpXIHI/v0/1200x-1.jpg",
       description: "Australians are warm and welcoming, hardworking and creative. We're one of the most multicultural countries in the world, and home to the world's oldest living culture.",
-      weather: "hot",
+      weather: "26°C",
       showDropdown: false
-    }
+    }, 
+    {
+      title: "Canada",
+      image: "https://a.cdn-hotels.com/gdcs/production159/d204/01ae3fa0-c55c-11e8-9739-0242ac110006.jpg",
+      description: "No matter which part of Canada you are visiting you'll find people, culture and communities that encapsulate the past of the country. There is a rich indigenous culture.",
+      weather: "20°C on the coast, 25°C in the middle",
+      showDropdown: false
+    },
+    {
+        title: "Japan",
+        image: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/26/97/39/7f/caption.jpg?w=1200&h=-1&s=1&cx=1920&cy=1080&chk=v1_f31158e4bb953d28a308",
+        description: "Japan is one of the oldest civilizations and has a beautiful and diverse history. There is stunning, diverse scenery with mountains and breathtaking views.",
+        weather: "26°C",
+        showDropdown: false
+      },
+      {
+        title: "Greece",
+        image: "https://media.timeout.com/images/105859749/750/422/image.jpg",
+        description: "Greece is famous for its amazing beaches and clear-blue waters. In addition, there is the perfect beauty of Cycladic architecture, incredible sunset spots, and warm summer weather.",
+        weather: "24°C ",
+        showDropdown: false
+      },
+      {
+        title: "Thailand",
+        image: "https://a.cdn-hotels.com/gdcs/production146/d585/aa60115c-bdfc-479f-88ba-5cb0f15a5af8.jpg?impolicy=fcrop&w=800&h=533&q=medium",
+        description: "Thailand is a country with a diverse range of landscapes and natural wonders waiting to be explored. Thailand is justifiably famous for its amazing islands and beaches.",
+        weather: "35°C ",
+        showDropdown: false
+      },
+      {
+        title: "France",
+        image: "https://www.commonwealthfund.org/sites/default/files/styles/countries_hero_desktop/public/country_image_France.jpg?h=007ba93e&itok=Uk8SO93w",
+        description: "It is home to some of the most varied and breathtaking scenery in the world. From the snow-capped peaks of the Alps to the sun-drenched beaches of the Mediterranean.",
+        weather: "20°C ",
+        showDropdown: false
+      }
   ]);
 
   const handleCardClick = index => {
